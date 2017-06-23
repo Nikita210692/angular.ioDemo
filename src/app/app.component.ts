83216151df4bf488438d2ceaa0336473014a4a2a
Nikita <nikita.jindal21@gmail.com>
2017-06-23 17:17:02 +0530

    for initial setup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+/**
+ * Created by nikita on 23/6/17.
+ */
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My First Angular App';
+  title = 'Tour of Heroes';
 }
---
+++ src/app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by nikita on 20/6/17.
+ */
+export class Hero {
+  id: number;
+  name: string;
+}
